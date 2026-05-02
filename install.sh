8ec37e2bab3e8a9c347398ea3f54a1bacb37eeb3
Daniel Patrick Fruzzetti <daniel@dfracknstack.com>
2026-05-02 08:08:01 -0700

    After 40 more hours of work we are deeply proud to present an EXCEEDINGLY fast curation experience even for large videos.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 🛸 MemoryBox Appliance Genesis Installer [v2.2.6]
+# 🛸 MemoryBox Appliance Genesis Installer [v2.2.19]
 # Purpose: Zero-touch transformation of a fresh Ubuntu server into a MemoryBox Appliance.
 
 set -e
